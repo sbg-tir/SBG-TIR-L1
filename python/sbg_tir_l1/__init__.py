ffbd7f7732d2ed4749acf007621fd742ff98dc8e
Mike Smyth <Mike.M.Smyth@jpl.nasa.gov>
2025-08-12 09:42:28 -0700

    Update init

--- python/sbg_tir_l1/__init__.py
+++ python/sbg_tir_l1/__init__.py
@@ -1,22 +1,19 @@
 # Just import any files we find in this directory, rather than listing
 # everything.
 
-# ***** Note
-# This init is just used for local testing. In particular, it doesn't load the
-# cython stuff. There is another __init__ file found at ../init/__init__.py that
-# is used for the full installed library. The purpose of this init it to
-# be able to locally run tests and do development without installing the full
-# system.
-
 from __future__ import absolute_import
-import os
-import re
-import glob
+import os as _os
+import re as _re
+import glob as _glob
 from .version import __version__
 from sbg_tir_l1_swig import *  # type: ignore
 
-for i in glob.glob(os.path.dirname(__file__) + "/*.py"):
-    mname = os.path.basename(i).split(".")[0]
+for _i in _glob.glob(os.path.dirname(__file__) + "/*.py"):
+    mname = _os.path.basename(_i).split(".")[0]
     # Don't load ipython, which is ipython magic extensions, or unit tests
-    if not mname == "ipython" and not re.search("_test", mname):
+    if not mname == "ipython" and not _re.search("_test", mname):
         exec("from .%s import *" % mname)
+del _i
+del _re
+del _os
+del _glob
